f12d86414797acafa2091dbe29b2ac8cdb945e4d
Jibin George <jibingeorge1964@gmail.com>
2020-05-15 14:27:22 +0000

    Matched schema standards and provided required field for title and description, made Jobs to Job, timestamps added

--- models/Jobs.js
+++ models/Jobs.js
@@ -3,67 +3,20 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/jobs');
 
 const JobSchema = new mongoose.Schema({
-    id : {
-        type : String, 
-        required : true
-    },
-    title : {
-        type : String,
-        required : true
-    },
-    description : {
-        type : String, 
-        required : true
-    },
-    date_from : {
-        type : Date,
-        timestamps : true,
-        required : true
-    },
-    date_to : {
-        type : Date,
-        timestamps : true,
-        required : true
-    },
-    time_from : {
-        type : Date,
-        timestamps : true,
-        required : true
-    },
-    time_to : {
-        type : Date,
-        timestamps : true,
-        required : true
-    },
-    skill : {
-        type: Schema.Types.ObjectId,
-        ref: 'Skills' 
-    },
-    status : {
-        type: String,
-        enum : [active, pending, commit, completed, rejected],
-        default: 'active'
-    },
-    posted_by : { 
-        type: Schema.Types.ObjectId,
-        ref: 'Users' 
-    },
-    commited_by : { 
-        type: Schema.Types.ObjectId, 
-        ref: 'Users' 
-    },
-    requests : [{ 
-        type : Schema.Types.ObjectId, 
-        ref: 'User' 
-    }],
-    created_at : {
-        type : Date,
-        timestamps : true
-    },
-    updated_at :{
-        type : Date,
-        timestamps : true
-    }
+    id : {type : String, required : true},
+    title : {type : String},
+    description : {type : String, required : true},
+    date_from : {type : Date, required : true},
+    date_to : {type : Date, required : true},
+    time_from : {type : Date, required : true},
+    time_to : {type : Date, required : true},
+    skill : { type: Schema.Types.ObjectId, ref: 'Skills' },
+    status : {type: String,enum : [active, pending, commit, completed, rejected],default: 'active'},
+    posted_by : { type: Schema.Types.ObjectId, ref: 'Users' },
+    commited_by : { type: Schema.Types.ObjectId, ref: 'Users' },
+    requests : [{ type : Schema.Types.ObjectId, ref: 'User' }],
+    created_at     : { type : Date, timestamps : true},
+    updated_at     : { type : Date, timestamps : true}
 
 });
 
