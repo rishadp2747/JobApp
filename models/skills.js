d5d1320e91cb8229808af5a5f41a1f9dc652e16a
nihal_ansar <nihalansar9008@gmail.com>
2020-05-15 21:47:50 +0530

    Added little styling

--- models/skills.js
+++ models/skills.js
@@ -1,17 +1,27 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-
 /* Creating connection with mongoDB server */
 mongoose.connect('mongodb://localhost:27017/jobapp');
 
-     /* Defining the Schema for Skills model */
-        const SkillSchema = new Schema({
-        	
-        	title          : { type : String, required:true},
-        	basic_charge   : { type : Decimal128, required:true},
-        	hourly_charge  : { type : Decimal128, required:true},
-        	created_by     : { type : ObjectId, ref: "Admins"}},
-                             { timestamps : true });
+/* Defining the Schema for Skills model */
+const SkillSchema = new Schema({
+    title          : { 
+                     type : String, 
+                     required:true
+                     },
+    basic_charge   : { 
+                     type : Decimal128, 
+                     required:true
+                     },
+    hourly_charge  : { 
+                     type : Decimal128, 
+                     required:true
+                     },
+    created_by     : { 
+                     type : ObjectId, 
+                     ref: "Admins"
+                     }},
+    { timestamps : true });
 
 /* Creating the Skills model */
 module.exports =  mongoose.model('Skill', SkillSchema);
