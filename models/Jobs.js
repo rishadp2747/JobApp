cdde1baf0ace3e8f61b51b35c6c9dd00d9b83b98
Jibin George <jibingeorge1964@gmail.com>
2020-05-15 14:03:37 +0000

    Update Jobs.js

--- models/Jobs.js
+++ models/Jobs.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/jobs', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/jobs');
 
 const JobSchema = new mongoose.Schema({
     id : {
