2de0e9eb358d0ee5df57a38473230394c7692cb4
nihal_ansar <nihalansar9008@gmail.com>
2020-05-15 22:05:48 +0530

    issues resolved

--- models/skills.js
+++ models/skills.js
@@ -1,7 +1,5 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-/* Creating connection with mongoDB server */
-mongoose.connect('mongodb://localhost:27017/jobapp');
 
 /* Defining the Schema for Skills model */
 const SkillSchema = new Schema({
@@ -20,7 +18,8 @@ const SkillSchema = new Schema({
     created_by     : { 
                      type : ObjectId, 
                      ref: "Admins"
-                     }},
+                     }
+                     },
     { timestamps : true });
 
 /* Creating the Skills model */
