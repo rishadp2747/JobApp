2cef2fa28ae4c775b77255cb66b2a42a9fbe7c77
nihal_ansar <nihalansar9008@gmail.com>
2020-05-15 19:35:53 +0530

    updated timestamps section

--- models/skills.js
+++ models/skills.js
@@ -11,8 +11,9 @@ mongoose.connect('mongodb://localhost:27017/jobapp');
         	title          : { type : String, required:true},
         	basic_charge   : { type : Decimal128, required:true},
         	hourly_charge  : { type : Decimal128, required:true},
-        	created_by     : { type: ObjectId, ref: "Admins"},
-            timestamps     : true                
+        	created_by     : { type : ObjectId, ref: "Admins"},
+            created_at     : { type : Date, timestamps : true},
+            updated_at     : { type : Date, timestamps : true}                
         
         });
 
