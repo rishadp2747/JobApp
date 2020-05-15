58a97f198696eeeba8dd69f00dff0b9cfb2de0c4
Jibin George <jibingeorge1964@gmail.com>
2020-05-15 16:39:32 +0000

    updated schema standard

--- models/Jobs.js
+++ models/Jobs.js
@@ -1,18 +1,63 @@
 const mongoose = require('mongoose');
 
 const JobSchema = new mongoose.Schema({
-    id : {type : String, required : true},
-    title : {type : String},
-    description : {type : String, required : true},
-    date_from : {type : Date, required : true},
-    date_to : {type : Date, required : true},
-    time_from : {type : Date, required : true},
-    time_to : {type : Date, required : true},
-    skill : { type: Schema.Types.ObjectId, ref: 'Skills' },
-    status : {type: String,enum : [active, pending, commit, completed, rejected],default: 'active'},
-    posted_by : { type: Schema.Types.ObjectId, ref: 'Users' },
-    commited_by : { type: Schema.Types.ObjectId, ref: 'Users' },
-    requests : [{ type : Schema.Types.ObjectId, ref: 'Users' }],
+    
+    id : {
+        type : String, 
+        required : true
+        
+    },
+    title : {
+        type : String
+        
+    },
+    description : {
+        type : String, 
+        required : true
+        
+    },
+    date_from : {
+        type : Date, 
+        required : true
+        
+    },
+    date_to : {
+        type : Date, 
+        required : true
+        
+    },
+    time_from : {
+        type : Date, 
+        required : true
+        
+    },
+    time_to : {
+        type : Date, 
+        required : true
+        
+    },
+    skill : { 
+        type: Schema.Types.ObjectId, 
+        ref: 'Skills'
+    },
+    status : {
+        type: String,
+        enum : [active, pending, commit, completed, rejected],
+        default: 'active'
+    },
+    posted_by : {
+        type: Schema.Types.ObjectId, 
+        ref: 'Users' 
+    },
+    commited_by : { 
+        type: Schema.Types.ObjectId, 
+        ref: 'Users' 
+    },
+    requests : [{ 
+        type : Schema.Types.ObjectId, 
+        ref: 'Users' 
+    }],
+    
 },{
     timestamps : true
 });
