2a56e7da393cd0349cc4fbaf40350b71ffcfd77a
nihal_ansar <nihalansar9008@gmail.com>
2020-05-15 18:15:36 +0530

    added router for jobs

---
+++ routes/api.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var router = express.Router();
+var jobs = require('../models/jobs');
+var bodyParser = require('body-parser');
+
+/* for /jobs */
+router
+/* To list all the jobs present in the server */
+.get('/jobs', function(req, res, next) {
+ jobs.find()
+     .then(function(doc) {
+         res.json({success : true, data : doc, message : "Jobs listed Successfully"});
+     })
+     .catch((err) => 
+         res.json({success : false, data : err, message : "Jobs listing Failed"})
+     	);
+})
+
+/* To list the data of a particular job using its id */
+.get('/jobs/:jobId', function(req, res, next) {
+ jobs.findById(req.params.jobId)
+     .then(function(doc) {
+         res.json({success : true, data : doc, message : "Jobs listed Successfully"});
+     })
+     .catch((err) => 
+         res.json({success : false, data : err, message : "Jobs listing Failed"})
+     	);
+})
+
+/* To add new job to the DB */
+.post('/jobs', function(req, res, next) {
+        var item = req.body;
+        console.log(item);
+  var data = new jobs(item);
+  data.save()
+      .then(()=> res.json({success : true, message : "Job Successfully added"}))
+      .catch((err) => 
+         res.json({success : false, data : err, message : "Job Adding Failed"})
+     	);
+})
+
+/* To update the data of a particular job using its id */
+.put('/jobs/:jobId', function(req, res, next) {
+	var id = {_id : req.params.jobId};
+ jobs.findByIdAndUpdate(id, req.body)
+     .then(function(doc) {
+         res.json({success : true, message : "Job Updated Successfully"});
+     })
+     .catch((err) => 
+         res.json({success : false, data : err, message : "Job Updation Failed"})
+     	);
+})
+
+/* To delete a particular job using its id */
+.delete('/jobs/:jobId', function(req, res, next) {
+ jobs.findByIdAndDelete(req.params.jobId)
+     .then(function(doc) {
+         res.json({success : true, message : "Job Successfully Deleted"});
+     })
+     .catch((err) => 
+         res.json({success : false, data : err, message : "Job Deletion Failed"})
+     	);
+});
+
+module.exports = router;
