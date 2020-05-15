5e9ac862bb1a9f64b2aba69d31f2a3fe8e0fc26a
Berlin <berlin@professor.professor>
2020-05-15 19:24:27 +0530

    second commit of JobsModel

--- models/Jobs.js
+++ models/Jobs.js
@@ -4,26 +4,63 @@ mongoose.set('useCreateIndex', true)
 mongoose.connect('mongodb://localhost:27017/jobs', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const JobSchema = new mongoose.Schema({
-    id : {type : String, required : true},
-    title : {type : String},
-    description : {type : String, required : true},
-    date_from : {type : Date},
-    date_to : {type : Date},
-    time_from : {type : Date},
-    time_to : {type : Date},
-    skill : { type: Schema.Types.ObjectId, ref: 'Skills' },
+    id : {
+        type : String, 
+        required : true
+    },
+    title : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String, 
+        required : true
+    },
+    date_from : {
+        type : Date,
+        timestamps : true
+    },
+    date_to : {
+        type : Date,
+        timestamps : true
+    },
+    time_from : {
+        type : Date,
+        timestamps : true
+    },
+    time_to : {
+        type : Date,
+        timestamps : true
+    },
+    skill : {
+        type: Schema.Types.ObjectId,
+        ref: 'Skills' 
+    },
     status : {
         type: String,
-        enum : [active, pending, commit, completed, rejected],
-        default: 'active'
-    }    
-    ,
-    posted_by : { type: Schema.Types.ObjectId, ref: 'Users' },
-    commited_by : { type: Schema.Types.ObjectId, ref: 'Users' },
-    requests : [{ type : Schema.Types.ObjectId, ref: 'User' }],
-    created_at : {type : Date},
-    updated_at :{type : Date}
+        enum : [active, pending, commit, completed, rejected],default: 'active'
+    },
+    posted_by : { 
+        type: Schema.Types.ObjectId,
+        ref: 'Users' 
+    },
+    commited_by : { 
+        type: Schema.Types.ObjectId, 
+        ref: 'Users' 
+    },
+    requests : [{ 
+        type : Schema.Types.ObjectId, 
+        ref: 'User' 
+    }],
+    created_at : {
+        type : Date,
+        timestamps : true
+    },
+    updated_at :{
+        type : Date,
+        timestamps : true
+    }
 
 });
 
-module.exports = require('Jobs',JobSchema);
+module.exports = require('Job',JobSchema);
