699d0bd0b0934be7b3951db2f01f47ce8c8a9aec
Jibin George <jibingeorge1964@gmail.com>
2020-05-16 09:25:33 +0000

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var AdminsRouter = require('./routes/api/Admins');
+var AdminsRouter = require('./routes/Admins');
 
 var app = express();
 
