c79d9257b0448353ed9200655c1d20b2d1b33817
jopaul23 <jopauljoy999@gmail.com>
2020-05-16 19:51:52 +0530

    skills routing file name changed from api.js to skills.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api',skillsRouter);
+app.use('/skills',skillsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
