3518a5ba259c84dab25a94e853a2359d3c2df057
Rishad P <rishadp2747@gmail.com>
2020-05-20 17:24:18 +0530

    providers

--- serviceProviders/mailer.js
+++ serviceProviders/mailer.js
@@ -4,19 +4,8 @@ exports.emailVerifier = (req, res, next) => {
     return transporter.sendMail({
         from : '"Email Verifier" <mail@fcs.net.in>',
         to : req.body.email,
-        subject : ""
+        subject : process.env.APP_NAME+" : email verification",
+
     })
 }
 
-
-
-let x = transporter.sendMail({
-    from: '"mail" <mail@fcs.net.in>', // sender address
-    to: "rishadp2747@gmail.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  } ,(err) => {
-    console.log(err);
-  });
-  
