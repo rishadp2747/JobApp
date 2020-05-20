1f818c9f55fda7fb6826b1bd6300eb48276ca21a
Rishad P <rishadp2747@gmail.com>
2020-05-20 17:40:49 +0530

    email verifier

--- serviceProviders/mailer.js
+++ serviceProviders/mailer.js
@@ -1,11 +1,22 @@
-transporter = require('../config/mail');
+const transporter = require('../config/mail');
+const ejs = require('ejs');
 
-exports.emailVerifier = (req, res, next) => {
-    return transporter.sendMail({
-        from : '"Email Verifier" <mail@fcs.net.in>',
-        to : req.body.email,
-        subject : process.env.APP_NAME+" : email verification",
-
-    })
+exports.emailVerifier = (user, otp, mail, next) => {
+    ejs.renderFile(__dirname + "/vendor/mail/emailVerification.ejs", { app: process.env.APP_NAME, name: user, otp : otp}, (err, data) => {
+        if(err){
+            console.log(err);
+            next(err);
+        }else{
+            transporter.sendMail({
+                from: '"FCS OTP Verifier" <mail@fcs.net.in>', // sender address
+                to: mail, // list of receivers
+                subject: process.env.APP_NAME+" : email verification", // Subject line
+                html: data, // html body
+            } ,(err) => {
+                console.log(err);
+                next(err);
+            });
+        }
+    });
 }
 
