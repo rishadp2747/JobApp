309e290ca0f8f0c5a5bfd41aa64be1435105ba79
nihal_ansar <nihalansar9008@gmail.com>
2020-05-31 00:13:39 +0530

    added skill adding endpoint

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -47,6 +47,17 @@ userRouter.route('/delete')
     });
   });
 
+userRouter.route("/skill/add")
+.put(user.verifyUser, user.verifyPhone,  (req, res, next) => {
+                    user.skills.push(req.body.skills);
+                    user.save( (err) => {
+                        if(err){
+                            response.errorResponse(res, 400, err.name, err.message);
+                        }else{
+                            response.dataResponse(res, 200, job, 'Successfully added the skill');
+                        }
+                    });
+      });
 
 
 module.exports = userRouter;
